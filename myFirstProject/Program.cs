73c3e96df8d64a7af3779078e62241bc652581a6
thestarrynight17 <thestarrynight17@users.noreply.github.com>
2024-05-14 14:32:39 +0400

    firstNumber variable initialized

--- myFirstProject/Program.cs
+++ myFirstProject/Program.cs
@@ -6,7 +6,10 @@ namespace myFirstProject
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello World!");
+
+            int firstNumber = 25;
+
+          
         }
     }
 }
